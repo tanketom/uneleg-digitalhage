903476c411034f1356398b24a254aac767bd8cf3
tanketom <tanketom@gmail.com>
2021-03-22 14:41:02 +0100

    Update typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -11,7 +11,12 @@ Wordpress2016.overrideThemeStyles = () => {
 
 delete Wordpress2016.googleFonts
 
-const typography = new Typography(Wordpress2016)
+const typography = new Typography({
+  baseFontSize: '16px',
+  baseLineHeight: '28px',
+  headerFontFamily: ['Lora', 'sans-serif'],
+  bodyFontFamily: ['Source Sans Pro', 'serif'],
+})
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
